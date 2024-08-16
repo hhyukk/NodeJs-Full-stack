52bb72d330985b28987c8a7bfcf02d203a881650
hhyukk <hhyukk1273@gmail.com>
2024-08-16 17:28:18 +0900

    Middlewares - middleware는 무조건 model이 생서외기 전에 만들어야 한다 - mongoose의 middleware에서 this 키워드는 저장하고자 하는 문서를카리킴

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -23,14 +23,14 @@ export const getEdit = async (req, res) => {
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await VideoEncoder.exists({ _id: id });
+  const video = await Video.exists({ _id: id });
   if (!video) {
     return res.render('404', { pageTitle: 'Video not found' });
   }
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags.split(',').map((word) => (word.startsWith('#') ? word : `#${word}`)),
+    hashtags,
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -43,10 +43,11 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(',').map((word) => (word.startsWith('#') ? word : `#${word}`)),
+      hashtags,
     });
     return res.redirect('/');
   } catch (error) {
+    console.log(error);
     return res.render('upload', { pageTitle: 'Upload Video', errorMessage: error._message });
   }
 };
--- study.md
+++ study.md
@@ -454,6 +454,30 @@ https://mongoosejs.com/docs/models.html
 
 https://mongoosejs.com/docs/queries.html#queries
 
+## Middleware
+
+미들웨어(pre또는 post훅이라고도 불림)는 비동기 함수를 실행하는 동안 제어가 전달되는 함수입니다.
+
+몽구스는 document middleware, model middleware, aggregate middleware, query middleware 4가지 미들웨어가 있습니다.
+
+https://mongoosejs.com/docs/middleware.html#middleware
+
+### model middleware가 지원하는 기능
+
+document middleware함수에서 this는 현재 document를 참조합니다.
+
+https://mongoosejs.com/docs/middleware.html#types-of-middleware
+
+데이터베이스에 전체 비디오 삭제 -> db.videos.remove({})가 deprecated됐다고 뜨시는 분들은
+
+#### Pre
+
+```
+pre("save", async function ());
+```
+
+https://mongoosejs.com/docs/middleware.html#pre
+
 ## MongoDB의 collection이름이 Video가 아닌 videos인 이유
 
 Mongoose는 자동으로 모델을 찾고, 해당 모델의 이름을 따서 소문자+뒤에 s(복수형)을 붙여 컬렉션을 생성합니다.
