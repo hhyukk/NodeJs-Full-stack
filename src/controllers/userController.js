544192125801cea66febfd8caa6506e5434092e9
hhyukk <hhyukk1273@gmail.com>
2024-10-28 22:25:01 +0900

    Change Password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -182,4 +182,40 @@ export const postEdit = async (req, res) => {
   req.session.user = updatedUser;
   return res.redirect('/users/edit');
 };
+
+export const getChangePassword = (req, res) => {
+  if (req.session.user.githubId === true) {
+    return res.redirect('/');
+  }
+  return res.render('users/change-password', { pageTitle: 'Change Password' });
+};
+export const postChangePassword = async (req, res) => {
+  const {
+    session: {
+      user: { _id, password },
+    },
+    body: { oldPassword, newPassword, newPasswordConfirmation },
+  } = req;
+  const user = await User.findById(_id);
+  const ok = await bcrypt.compare(oldPassword, user.password);
+  if (!ok) {
+    return res.status(400).render('users/change-password', {
+      pageTitle: 'Change Passwordddd',
+      errorMessage: 'The current password is incorrect',
+    });
+  }
+  if (newPassword !== newPasswordConfirmation) {
+    return res.status(400).render('users/change-password', {
+      pageTitle: 'Change Passwordddd',
+      errorMessage: 'The password does not match the confirmation',
+    });
+  }
+  console.log('Old password', user.password);
+  user.password = newPassword;
+  console.log('New unhashed pw', user.password);
+  await user.save();
+  console.log('new pw', user.password);
+
+  return res.redirect('/users/logout');
+};
 export const see = (req, res) => res.send('See User');
