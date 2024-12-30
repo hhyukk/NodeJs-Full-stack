3080be1d8f9bc88e9ccf9336ec5100065cf4719a
hhyukk <hhyukk1273@gmail.com>
2024-12-31 01:28:04 +0900

    Play Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,24 @@
-console.log('video player');
+const video = document.querySelector('video');
+const playBtn = document.getElementById('play');
+const muteBtn = document.getElementById('mute');
+const time = document.getElementById('time');
+const volum = document.getElementById('volum');
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    playBtn.innerText = 'Pause';
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const handlePause = () => (playBtn.innerText = 'Play');
+const handlePlay = () => (playBtn.innerText = 'Pause');
+
+const handleMute = (e) => {};
+
+playBtn.addEventListener('click', handlePlayClick);
+muteBtn.addEventListener('click', handleMute);
+video.addEventListener('pause', handlePause);
+video.addEventListener('play', handlePlay);
