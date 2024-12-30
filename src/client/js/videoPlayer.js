fc5d9e7b17575645de22e5bce970dcebe37aaf6d
hhyukk <hhyukk1273@gmail.com>
2024-12-31 02:04:42 +0900

    Mute and unmute

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,7 @@ const video = document.querySelector('video');
 const playBtn = document.getElementById('play');
 const muteBtn = document.getElementById('mute');
 const time = document.getElementById('time');
-const volum = document.getElementById('volum');
+const volumRange = document.getElementById('volum');
 
 const handlePlayClick = (e) => {
   if (video.paused) {
@@ -11,14 +11,18 @@ const handlePlayClick = (e) => {
   } else {
     video.pause();
   }
+  playBtn.video.paused ? 'Play' : 'Pause';
 };
 
-const handlePause = () => (playBtn.innerText = 'Play');
-const handlePlay = () => (playBtn.innerText = 'Pause');
-
-const handleMute = (e) => {};
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? 'Unmute' : 'Mute';
+  volumRange.value = video.muted ? 0 : 0.5;
+};
 
 playBtn.addEventListener('click', handlePlayClick);
 muteBtn.addEventListener('click', handleMute);
-video.addEventListener('pause', handlePause);
-video.addEventListener('play', handlePlay);
