282469091a92e7e46959369d538cd0ec3ac7d2b9
hhyukk <hhyukk1273@gmail.com>
2025-01-01 23:19:10 +0900

    Duration and Current Time

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,9 @@
 const video = document.querySelector('video');
 const playBtn = document.getElementById('play');
 const muteBtn = document.getElementById('mute');
-const time = document.getElementById('time');
 const volumeRange = document.getElementById('volum');
+const currenTime = document.getElementById('currenTime');
+const totalTime = document.getElementById('totalTime');
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -17,14 +18,10 @@ const handlePlayClick = (e) => {
 };
 
 const handleMute = (e) => {
-  if (video.muted) {
-    video.muted = false;
-    video.volume = volumeValue;
-  } else {
-    video.muted = true;
-  }
+  video.muted = !video.muted;
   muteBtn.innerText = video.muted ? 'Unmute' : 'Mute';
   volumeRange.value = video.muted ? 0 : volumeValue;
+  if (!video.muted) video.volume = volumeValue;
 };
 
 const handleInputVolumeChange = (event) => {
@@ -52,7 +49,17 @@ const handleChangeVolumeChange = (event) => {
   }
 };
 
+const handleLoadedMetadata = () => {
+  totalTime.innerText = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  currenTime.innerText = Math.floor(video.currentTime);
+};
+
 playBtn.addEventListener('click', handlePlayClick);
 muteBtn.addEventListener('click', handleMute);
 volumeRange.addEventListener('input', handleInputVolumeChange);
 volumeRange.addEventListener('change', handleChangeVolumeChange);
+video.addEventListener('loadedmetadata', handleLoadedMetadata);
+video.addEventListener('timeupdate', handleTimeUpdate);
