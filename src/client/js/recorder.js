b364c89149bc3acde43b2ad07a0050d02cfb4111
hhyukk <hhyukk1273@gmail.com>
2025-01-07 20:36:29 +0900

    Downloading the File

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,8 +3,15 @@ const video = document.getElementById('preview');
 
 let stream;
 let recorder;
+let videoFile;
 
-const handleDownload = () => {};
+const handleDownload = () => {
+  const a = document.createElement('a');
+  a.href = videoFile;
+  a.download = 'MyRecording'; //비디로오 안 나오고 텍스트 등으로 나오면 .webm 등 확장자 추가
+  document.body.appendChild(a);
+  a.click();
+};
 
 const handleStop = () => {
   startBtn.innerText = 'Download Recording';
@@ -19,7 +26,7 @@ const handleStart = async () => {
 
   recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
-    const videoFile = URL.createObjectURL(event.data);
+    videoFile = URL.createObjectURL(event.data);
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
