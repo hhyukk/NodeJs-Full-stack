815d02df8d2adb988b1857e5a8bdd586236ad4be
hhyukk <hhyukk1273@gmail.com>
2025-01-08 22:29:18 +0900

    Transcode Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,5 @@
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+
 const startBtn = document.getElementById('startBtn');
 const video = document.getElementById('preview');
 
@@ -5,7 +7,12 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({ log: true });
+  await ffmpeg.load();
+  ffmpeg.FS('writeFile', 'recording.webm', await fetchFile(videoFile));
+  await ffmpeg.run('-i', 'recording.webm', '-r', '60', 'output.mp4');
+
   const a = document.createElement('a');
   a.href = videoFile;
   a.download = 'MyRecording'; //비디로오 안 나오고 텍스트 등으로 나오면 .webm 등 확장자 추가
@@ -27,6 +34,7 @@ const handleStart = async () => {
   recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
     videoFile = URL.createObjectURL(event.data);
+    console.log(videoFile);
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
