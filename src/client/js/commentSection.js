9c36f270f043a18836a41e85f33a250f7e981c89
hhyukk <hhyukk1273@gmail.com>
2025-01-20 21:02:04 +0900

    Comment Ids

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,19 +1,26 @@
 const videoContainer = document.getElementById('videoContainer');
 const form = document.getElementById('commentForm');
+const delBtn = document.getElementById('comment_delete');
 
-const addComment = (text) => {
+const addComment = (text, id) => {
   const videoComments = document.querySelector('.video__comments ul');
   const newComment = document.createElement('li');
+  newComment.dataset.id = id;
   newComment.className = 'video__comment';
   const icon = document.createElement('i');
   icon.className = 'fas fa-comment';
   const span = document.createElement('span');
   span.innerText = ` ${text}`;
+  const span2 = document.createElement('span');
+  span2.innerText = '❌';
   newComment.appendChild(icon);
   newComment.appendChild(span);
+  newComment.appendChild(span2);
   videoComments.prepend(newComment);
   //prepend(): 콘텐츠를 선택한 요소 내부의 시작 부분에서 삽입
 };
+
+const handleDeleteComment = () => {};
 
 const handleSubmit = async (event) => {
   event.preventDefault();
@@ -23,16 +30,17 @@ const handleSubmit = async (event) => {
   if (text === '') {
     return;
   }
-  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ text }),
   });
-  textarea.value = '';
-  if (status === 201) {
-    addComment(text);
+  if (response.status === 201) {
+    textarea.value = '';
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
   }
   // window.location.reload();
 };
@@ -40,3 +48,4 @@ const handleSubmit = async (event) => {
 if (form) {
   form.addEventListener('submit', handleSubmit);
 }
+delBtn.addEventListener('click', handleDeleteComment);
